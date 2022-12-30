554836bb7b6bbd69deec21dd17e7a959bf856c04
AleksandrMarkitan <0688138@gmail.com>
2022-12-31 01:56:40 +0200

    finished

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,15 @@
 const { User } = require("../models/user");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const gravatar = require("gravatar");
+const fs = require("fs/promises");
+const path = require("path");
+const Jimp = require("jimp");
+
 require("dotenv").config(); // чому у Богдана SECRET_KEY дістається без цієї строки (в мідлварі авторізації аналогічно)
+const { SECRET_KEY } = process.env;
 
 const { HttpError, ctrlWrapper } = require("../helpers/index");
-
-const { SECRET_KEY } = process.env;
 
 const signup = async (req, res) => {
   const { email, password } = req.body;
@@ -16,9 +20,18 @@ const signup = async (req, res) => {
   }
 
   const hashPassword = await bcrypt.hash(password, 10);
-  const newUser = await User.create({ ...req.body, password: hashPassword });
+  const avatarURL = gravatar.url(email);
+
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL,
+  });
   res.status(201).json({
-    user: { email: newUser.email, subscription: newUser.subscription },
+    user: {
+      email: newUser.email,
+      subscription: newUser.subscription,
+    },
   });
 };
 
@@ -69,10 +82,34 @@ const updateSubscription = async (req, res) => {
   res.json(`your subscription has been updated to ${req.body.subscription}`);
 };
 
+const avatarsDir = path.join(__dirname, "../", "public", "avatars");
+
+const updateAvatar = async (req, res) => {
+  const { _id } = req.user;
+  const { path: tempUpload, originalname } = req.file;
+  const filename = `${_id}_${originalname}`;
+  const resultUpload = path.join(avatarsDir, filename);
+
+  await fs.rename(tempUpload, resultUpload);
+  const avatarURL = path.join("avatars", filename);
+
+  await User.findByIdAndUpdate(_id, { avatarURL });
+
+  const image = await Jimp.read(resultUpload);
+  // Resize the image to width 250 and auto height.
+  // await image.resize(250, Jimp.AUTO);
+  await image.resize(250, 250);
+  // Save and overwrite the image
+  await image.writeAsync(resultUpload);
+
+  res.json({ avatarURL });
+};
+
 module.exports = {
   signup: ctrlWrapper(signup),
   login: ctrlWrapper(login),
   getCurrent: ctrlWrapper(getCurrent),
   logout: ctrlWrapper(logout),
   updateSubscription: ctrlWrapper(updateSubscription),
+  updateAvatar: ctrlWrapper(updateAvatar),
 };
